ec1108eda8aec045d0213d081c71f79dfa815e4e
Sweren <34696039+Sweren@users.noreply.github.com>
2021-02-27 01:09:02 -0500

    Update files

--- resources/[discord]/RichPresence/config.lua
+++
@@ -1,6 +0,0 @@
-Config = {}
-Config.ClientID = 701562774614048849  -- Put your discord bot client id here
-Config.PlayerCount = 64
-Config.PlayerText = "Ciudadanos" -- Player text. Example Players 10/32
-Config.ResourceTimer = 5 -- Time in witch resource updates in seconds
-Config.UseESXIdentity = true -- Uses ESX Identity name not steam name
--- resources/[discord]/UsmekistanQueue/client.lua
+++
@@ -1,9 +0,0 @@
-Citizen.CreateThread(function()
-	while true do 
-		Citizen.Wait(0);
-		if NetworkIsSessionStarted() then 
-			TriggerServerEvent('DiscordQueue:Activated'); -- They got past queue, deactivate them in it
-			return 
-		end
-	end
-end)
