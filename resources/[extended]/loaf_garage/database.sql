33291d71189790bca8ca87b172fcd0d5333d774b
Sweren <34696039+Sweren@users.noreply.github.com>
2021-03-02 09:02:40 -0500

    Adjust.

--- resources/[extended]/loaf_garage/database.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `owned_vehicles` ADD COLUMN `damages` varchar(255) NOT NULL DEFAULT '{}';
-ALTER TABLE `owned_vehicles` ADD COLUMN `state` BOOLEAN NOT NULL DEFAULT TRUE;
-ALTER TABLE `owned_vehicles` ADD COLUMN `garage` VARCHAR(255) NOT NULL DEFAULT 'square';
-ALTER TABLE `owned_vehicles` ADD COLUMN `job` varchar(20) NOT NULL DEFAULT '';
-ALTER TABLE `owned_vehicles` ALTER `job` SET DEFAULT '';
